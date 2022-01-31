81ebf808b79eb6dabc45b2a5e49de6825b4be83b
Fabian <fabianrojas.sab@gmail.com>
2022-01-30 21:10:46 -0500

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import { Container, Row } from 'react-bootstrap';
+
+function Projects(){
+    return(
+        <Container fluid className="App-body">
+            <Row>
+                <h2>Escultor, pintor y amante del arte</h2>
+            </Row> 
+        </Container>
+    );
+}
+
+export default Projects ;
