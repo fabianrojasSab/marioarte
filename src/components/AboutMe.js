13b9f39f52d82cc728567c7ce6c740b9c1cc5457
fabian <fabianrojas.sab@gmail.com>
2023-02-12 19:17:51 -0500

    fase 1, finalizada y subida a produccion

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,14 +5,14 @@ function AboutMe(){
     return(
         <Container fluid className="App-body-about App-body">
             <Row>
-                <Col>
+                <Col lg={6} sm={12}>
                 <h2>Mario Suarez</h2>
                 <p>
                 Artista plástico nacido en Pitalito Huila en Julio de 1984, autodidacta con 21 años de experiencia en dibujo, pintura y escultura, en su obra al carboncillo se destaca el retrato, en su pintura podemos observar un universo infinito compuesto de figuras orgánicas en una danza de colores vibrantes en una narrativa acerca de la complejidad de la mente de su propio ser, en su escultura se destaca la figura humana cómo elemento principal, entre sus obras se encuentran monumentos públicos, monumentos en colecciones privadas.
 Su obra se encuentra en países como estados unidos, México, argentina, panamá, España y londres.
                 </p>
                 </Col>
-                <Col>
+                <Col lg={6} sm={12}>
                     <img src={img1}  width="100%"  height="100%" className="img-fluid" alt='foto' />
                 </Col>
             </Row> 
